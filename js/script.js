311709b8ced8cd25b4a3eeca929701668c73429a
kjoh-dev <kevinjoh85@gmail.com>
2021-09-24 00:06:48 -0400

    Project started. Created index.html, script.js and style.css files.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+const gridContainer = document.querySelector(".grid-container");
+const gridWidthButton = document.querySelector(".grid-width-button");
+const gridWidthText = document.querySelector("input[type='text']");
+
+// gridWidthButton.addEventListener("click", )
+
+// let gridWidth = Number(gridWidthText.value);
+
+
+
+
+
+
+
+
+
+
+function createGrid(width){
+    gridContainer.style.display = "grid";
+}
