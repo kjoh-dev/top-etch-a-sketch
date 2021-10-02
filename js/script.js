7346264d113267136485cb5c434d2130519407b9
kjoh-dev <kevinjoh85@gmail.com>
2021-10-02 13:50:56 -0400

    All basic features implemented: single/random color modes and solid/darken/lighten paint modes. index.html: relevant input elements added. style.cs: added sliding menu to host new input elements. script.js: new features implemented.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,186 @@
+//HTML References
 const gridContainer = document.querySelector(".grid-container");
-const createGridButton = document.querySelector("input[type='button']");
+// const createGridButton = document.querySelector("#create");
+const inputElems = document.querySelectorAll("input");
+
+//Paint Mode:
+const SOLID = "solid";
+const DARKEN = "darken";
+const LIGHTEN = "lighten";
+
+//Color Mode:
+const SINGLE = "single";
+const RANDOM = "random";
+
+//Brush Thickness?
+
+//Variable Declarations/Initializations
+let gridSize = 16;
+let colorMode = SINGLE;
+let paintMode = SOLID;
+let selectColor = "#000000";
+
+initializeInputElems();
 
 
-createGridButton.addEventListener("click", createGrid);
 
-function colorCell(e){
-    e.target.style.backgroundColor = "black";
+
+
+//Function Definitions
+function initializeInputElems(){
+    for (let i = 0; i < inputElems.length; i++) {
+        const inputElem = inputElems[i];
+        if(!(inputElem instanceof HTMLElement)) return;
+    
+        if(inputElem.type === "number"){
+            inputElem.defaultValue = 16;
+            inputElem.max = 100;
+            inputElem.min = 1;
+            inputElem.addEventListener("change", function(){
+                if(isNaN(Number(inputElem.value))) {
+                    alert("Please input an integer value from 1 to 100");
+                    return;
+                }
+                gridSize = Number(inputElem.value);
+            });
+        }
+    
+        if(inputElem.type === "color"){
+            inputElem.addEventListener("change", function(){selectColor = inputElem.value})
+            selectColor = inputElem.value;
+        }
+    
+        if(inputElem.type === "button"){
+            const id = inputElem.id;
+            switch(id){
+                case "create":
+                    inputElem.addEventListener("click", createGrid);
+                    break;
+                case "single":
+                    inputElem.addEventListener("click", function(){colorMode = SINGLE})
+                    break;
+                case "random":
+                    inputElem.addEventListener("click", function(){colorMode = RANDOM})
+                    break;
+                case "solid":
+                    inputElem.addEventListener("click", function(){paintMode = SOLID})
+                    break;
+                case "darken":
+                    inputElem.addEventListener("click", function(){paintMode = DARKEN})
+                    break;
+                case "lighten":
+                    inputElem.addEventListener("click", function(){paintMode = LIGHTEN})
+                    break;
+                default:
+                    console.log(`Error in initializations - invalid id: ${id}`);
+                    break;
+            }
+        }
+    }
 }
+
+function getFilterValue(htmlElement, filterName){
+    if(!(htmlElement instanceof HTMLElement)) return;
+
+    const filters = window.getComputedStyle(htmlElement).getPropertyValue("filter");
+
+    let propertyValue;
+
+    switch(filterName){
+        // case "grayscale":
+        //     propertyValue = filters.match(/grayscale\((.*?)%\)/);
+        //     break;
+        // case "hue-rotate":
+        //     propertyValue = filters.match(/hue-rotate\((.*?)d/);
+        //     break;
+        case "brightness":
+            propertyValue = filters.match(/brightness\((.*?)\)/);
+            break;
+        default:
+            console.log(`Error in getFilterValue - filtername: ${filterName}`);
+            break;
+    }
+
+    return Number(propertyValue[1]);
+}
+
+function setFilterValue(htmlElement, filterName, filterValue){
+    if(!(htmlElement instanceof HTMLElement) && (typeof filterName !== "string") && (typeof filterValue !== "string")) return;
+
+    const prevFilters = window.getComputedStyle(htmlElement).getPropertyValue("filter");
+
+    let newFilters;
+    let regex;
+
+    switch(filterName){
+        // case "hue-rotate":
+        //     regex = /hue-rotate\(.*?\)/;
+        //     newFilters = prevFilters.replace(regex, `${filterName}(${filterValue}deg)`);          break;
+        case "brightness":
+            regex = /brightness\(.*?\)/;
+            newFilters = prevFilters.replace(regex, `${filterName}(${filterValue})`);
+            break;
+        default:
+            console.log(`Error in setFilterValue - filterName: ${filterName}`);
+            break;
+    }
+
+    htmlElement.style.filter = newFilters;
+}
+
+function modCellBrightness(cellElement){
+    if(!(cellElement instanceof HTMLElement)) return;
+    
+    let newBrightness = getFilterValue(cellElement, "brightness");
+    newBrightness *= 100;
+
+    switch(true){
+        case paintMode === SOLID:
+            newBrightness = 100;
+            break;
+        case paintMode === DARKEN:
+            newBrightness -= 10;
+            break;
+        case paintMode === LIGHTEN:
+            newBrightness += 10;
+            break;
+        default:
+            console.log(`Error in setCellBrightness - paintMode: ${paintMode}`);
+            break;
+    }
+
+    newBrightness = (newBrightness > 100) ? 100 : 
+    (newBrightness < 0) ? 0 : newBrightness;
+
+    newBrightness /= 100;
+    setFilterValue(cellElement, "brightness", newBrightness)
+}
+
+function setCellColor(cellElement){
+    if(!(cellElement instanceof HTMLElement)) return;
+
+    if(colorMode === SINGLE){
+        const prevColor = window.getComputedStyle(cellElement).getPropertyValue("background-color");
+        if(prevColor != selectColor) cellElement.style.backgroundColor = selectColor;
+
+    } else {
+        const randomColor = "#"+(Math.floor(Math.random()*16777215)+1).toString(16).padStart(2, "0");
+        cellElement.style.backgroundColor = randomColor;
+    }
+}
+
+function paintCell(e){
+    cell = e.target;
+    if(!(cell instanceof HTMLElement)) return;
+
+    setCellColor(cell);
+
+    modCellBrightness(cell);
+
+}
+
+
+
 
 function clearGrid(){
     for (let i = gridContainer.childElementCount-1; i >= 0; i--) {
@@ -17,45 +191,28 @@ function clearGrid(){
 }
 
 function createGrid(e){
-    
-    const gridSizeText = prompt("Please specify grid size in number of cells per side (Max: 100)", "16");
 
-    if (gridSizeText === null) {
+    const createGridButton = e.target;
+    if(!(createGridButton instanceof HTMLElement) && createGridButton.id === "create") return;
+
+    if(gridSize > 100 || gridSize < 2) {
+        alert("Please specify grid size in number of cells per side from 2 to 100.");
         return;
     }
 
-    let gridSize = Number(gridSizeText);
+    if (gridContainer.childElementCount > 0) clearGrid();
 
-    if (gridSize === NaN) {
-        return;
-    } 
-    if (gridSize > 100){
-        gridSize = 100;
-        alert("Specified size is greater than the max size of 100. Size of 100 shall be used instead.");
-    }
-
-    if (gridContainer.childElementCount > 0){
-        clearGrid();
-    }
-
-    if (e.target === createGridButton) {
-        for (let j = gridSize; j > 0; j--) {
-            for (let i = gridSize; i > 0; i--) {
-                const cell = document.createElement("div");
-                cell.style.color = "red";
-                cell.style.border = "1px solid black";
-                cell.addEventListener("mouseover", colorCell);
-                gridContainer.appendChild(cell);
-            }
+    for (let j = gridSize; j > 0; j--) {
+        for (let i = gridSize; i > 0; i--) {
+            const cell = document.createElement("div");
+            cell.style.border = "1px solid gray";
+            cell.classList.add("cell");
+            cell.addEventListener("mouseenter", paintCell);
+            gridContainer.appendChild(cell);
         }
-
-        gridContainer.style.display = "grid";
-        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
-        console.log("grid created");
     }
-    else{
-        console.log("grid not created");
-    }
+
+    gridContainer.style.display = "grid";
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 }
